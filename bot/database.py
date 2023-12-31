ce6a937357ba26234a2478a0f4d0438832e93c96
root <root@xxxuy.>
2023-12-31 18:41:04 +0500

    refactoring code

--- bot/database.py
+++ bot/database.py
@@ -1,69 +1,56 @@
+
 # bot/database.py
 
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, DateTime, ForeignKey
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import declarative_base, sessionmaker, scoped_session
+from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime
+import logging
 
 DATABASE_URL = "sqlite:///bot_database.db"
 engine = create_engine(DATABASE_URL, echo=False)
 Session = sessionmaker(bind=engine)
-metadata = MetaData()
+Base = declarative_base()
 
-# Описание таблиц
-users_table = Table('users', metadata,
-                    Column('id', Integer, primary_key=True),
-                    Column('username', String),
-                    Column('first_name', String),
-                    Column('last_name', String)
-                    )
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
 
-queries_table = Table('queries', metadata,
-                      Column('id', Integer, primary_key=True),
-                      Column('user_id', Integer, ForeignKey('users.id')),
-                      Column('text', String),
-                      Column('response', String),
-                      Column('timestamp', DateTime, default=datetime.utcnow)
-                      )
+class Query(Base):
+    __tablename__ = 'queries'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    text = Column(String)
+    response = Column(String)
+    timestamp = Column(DateTime, default=datetime.utcnow)
 
-metadata.create_all(engine)
+Base.metadata.create_all(engine)
 
 class DatabaseManager:
     def __init__(self):
-        self.session = None  # Инициализируем атрибут здесь
         self.session = scoped_session(Session)
-        
+    
     def add_or_update_user(self, user_id, username, first_name, last_name):
-        with self.session() as session:
-            stmt = users_table.insert().values(
-                id=user_id,
-                username=username,
-                first_name=first_name,
-                last_name=last_name
-            )
-            try:
-                session.execute(stmt)
+        try:
+            with self.session() as session:
+                session.merge(User(id=user_id, username=username, first_name=first_name, last_name=last_name))
                 session.commit()
-            except IntegrityError:
-                session.rollback()
-                stmt = users_table.update().where(users_table.c.id == user_id).values(
-                    username=username,
-                    first_name=first_name,
-                    last_name=last_name
-                )
-                session.execute(stmt)
-                session.commit()
-
+        except SQLAlchemyError as e:
+            logging.error(f"Database error in add_or_update_user: {e}")
+            session.rollback()
+    
     def add_query(self, user_id, text, response):
-        with self.session() as session:
-            new_query = queries_table.insert().values(
-                user_id=user_id,
-                text=text,
-                response=response,
-                timestamp=datetime.utcnow()
-            )
-            session.execute(new_query)
-            session.commit()
+        try:
+            with self.session() as session:
+                new_query = Query(user_id=user_id, text=text, response=response)
+                session.add(new_query)
+                session.commit()
+        except SQLAlchemyError as e:
+            logging.error(f"Database error in add_query: {e}")
+            session.rollback()
 
     def __del__(self):
         self.session.remove()

3ddbf19b3ae22c3b3f954ab2f634eeef2c392af9
randomu3 <demiz99@mail.ru>
2023-12-31 19:22:55 +0500

    added somethings of formatted text of gpt

--- bot/main.py
+++ bot/main.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 from os import getenv, path
 import os
 import logging
+import re
 
 # Set up logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -54,6 +55,9 @@ def process_user_message(update: Update, context: CallbackContext) -> None:
     user_message = update.message.text
     user = update.message.from_user
 
+    # Log user's message
+    logging.info(f"User {user.id} ({user.username}): {user_message}")
+
     # Update user in database
     db_manager.add_or_update_user(user.id, user.username, user.first_name, user.last_name)
 
@@ -62,19 +66,40 @@ def process_user_message(update: Update, context: CallbackContext) -> None:
 
     # Get response from API
     response_text = hackergpt_api.send_message(context.user_data['message_history'])
-    update_message_history(context, 'assistant', response_text)
 
-    # Record query and response in database
-    db_manager.add_query(user.id, user_message, response_text)
+    # Log GPT's response
+    logging.info(f"GPT response to {user.id} ({user.username}): {response_text}")
 
-    # Send response to user
-    update.message.reply_text(response_text)
+    # Format code blocks in the response
+    formatted_response_text = format_code_block(response_text)
 
+    # Escape Markdown V2 special characters
+    escaped_response_text = escape_markdown_v2(formatted_response_text)
+
+    # Record query and response in the database
+    db_manager.add_query(user.id, user_message, escaped_response_text)
+
+    # Send formatted and escaped response to the user
+    update.message.reply_text(escaped_response_text, parse_mode='MarkdownV2')
+    
 # Update message history
 def update_message_history(context: CallbackContext, role: str, message: str) -> None:
     message_history = context.user_data.get('message_history', [])
     message_history.append({'role': role, 'content': message})
     context.user_data['message_history'] = message_history
+
+def escape_markdown_v2(text):
+    # Escape Markdown V2 special characters
+    escape_chars = '_*[]()~`>#+-=|{}.!'
+    return re.sub(r'([_*\[\]()~`>#\+=\-|{}\.!])', r'\\\1', text)
+
+def format_code_block(response_text):
+    # Pattern to find code blocks with language hints (e.g., ```python)
+    pattern = r'```(\w+)\s+(.+?)\s+```'
+    # Replace found patterns with triple backticks without language hints
+    # and ensure no leading or trailing spaces inside the code block
+    formatted_text = re.sub(pattern, r'```\n\2\n```', response_text, flags=re.DOTALL)
+    return formatted_text
 
 # Main function to set up and start the bot
 def main() -> None:
